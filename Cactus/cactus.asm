f007f48d2832139a7eab1ef7199b059ebfe071f5
Sebastián Orantes Chang <ater7991@gmail.com>
2018-10-24 16:02:00 -0600

    photocell code

--- Cactus/cactus.asm
+++ Cactus/cactus.asm
@@ -31,6 +31,11 @@ CON     EQU 0X47
 CON2    EQU 0X48
 CON3    EQU 0X49
 GROUPNUMBER	EQU 0x50
+
+waffles EQU 0x51
+CounterC EQU 0X52
+CounterB EQU 0X53
+CounterA EQU 0X54
 INICIO
 
 	ORG		0x00
@@ -58,14 +63,16 @@ START
 	call 	CLEANY      ;SET COUNTERS TO 0
 	call 	CLEANX
 	call 	CLEANBETA
-
+	MOVLW	0x00
+	MOVWF	VARPHOTOCELL
+;------------------------------------------------------------------------------
                 ;CONFIGURATION FOR THE PHOTOCELL 
- 	bcf STATUS,RP0 ;Ir banco 0
+	bcf STATUS,RP0 ;Ir banco 0
 	bcf STATUS,RP1
 	movlw b'01000001' ;A/D conversion Fosc/8
 	movwf ADCON0
 	;     	     7     6     5    4    3    2       1 0
-	; 1Fh ADCON0 ADCS1 ADCS0 CHS2 CHS1 CHS0 GO/DONE � ADON
+	; 1Fh ADCON0 ADCS1 ADCS0 CHS2 CHS1 CHS0 GO/DONE ? ADON
 	bsf STATUS,RP0 ;Ir banco 1
 	bcf STATUS,RP1
 	movlw b'00000111'
@@ -75,12 +82,19 @@ START
 	movlw b'00001110' ;A/D Port AN0/RA0
 	movwf ADCON1
 	;            7    6     5 4 3     2     1     0 
-	; 9Fh ADCON1 ADFM ADCS2 � � PCFG3 PCFG2 PCFG1 PCFG0
+	; 9Fh ADCON1 ADFM ADCS2 ? ? PCFG3 PCFG2 PCFG1 PCFG0
 	bsf TRISA,0 ;RA0 linea de entrada para el ADC
-	;clrf TRISB
+    	;Port A: display output
+    	bcf	TRISA, 1
+   	bcf	TRISA, 2
+    	bcf	TRISA, 3
+   	bcf	TRISA, 4
+    	bcf	TRISA, 5
+    	bcf	TRISE, 0
+    	bcf	TRISE, 1
+	clrf TRISB
 	bcf STATUS,RP0 ;Ir banco 0
 	bcf STATUS,RP1
-	clrf PORTB ;Limpiar PORTB   
 ;--------------------------------------------------------------------
 ;--------------------------------------------------------------------
 						;MAIN
@@ -104,6 +118,7 @@ SEARCH:
 	call	CLEANBETA   ;CLEAN COUNTBETA
 	call 	LOOPBETAREVERSE    ;RETURN TO ORIGINAL POSITION
     RETURN
+
 ;------------------------------------------------------------------
                         ;MOVE TO GREATER
 MOVEGREATER:
@@ -181,7 +196,8 @@ LOOPXREVERSEMAX:
 
 ;-------------------------------------------------------------------
 ;                   Photocell comparation
-COMPARE:
+;COMPARE:
+	;goto 	GET_PHOTOCELL
     ;TMPCELL = READ PHOTOCELL INF.
     ;ASIGNAR A TMPCELL EL NUMERO EN LA ESCALA DE LUZ (0-9)
 
@@ -189,15 +205,17 @@ COMPARE:
 	;positive     1		0
 	;zero		  1		1
 	;negative     0     0
+COMPARE:
     MOVF    VARPHOTOCELL,W
     SUBWF   TMPCELL, W ; W = VARPHOTOVELL - TMPCELL 
-    BTFSC   STATUS, C   ;IF VARPHOTOCELL < TMPCELL ; c=0 if negative
-    call    CHANGEPHOTOCELL    
-    return
+    BTFSS   STATUS, C   ;IF VARPHOTOCELL < TMPCELL ; c=0 if negative
+    call    CHANGEPHOTOCELL   
+	goto 	DimeElGordito 
+    ;return
 
 CHANGEPHOTOCELL:
     MOVLW   TMPCELL
-    MOVWF    VARPHOTOCELL  ;PHOTOCELL =  TMPCELL
+    MOVWF   VARPHOTOCELL  ;PHOTOCELL =  TMPCELL
     MOVLW   TMPY
     MOVWF   VARMAXY     ;VARMAXY = TMPY
     MOVLW   TMPX
@@ -236,7 +254,9 @@ LOOPY: ;LOOP i <= LIMITY
 	call 	EJEY
 	;INSERT LOGIC COMPARE VALUE OF PHOTOCELL & SAVE IT
     ;maybe add a delay here \o/
-    call    COMPARE
+    ;call    COMPARE
+	goto 	GET_PHOTOCELL
+DimeElGordito:
 
 	;MOVE ON Y
     INCF    COUNTY,1
@@ -282,111 +302,10 @@ Delay_0
 	return
 ;-------------------------------------------------------------------
 ;						UTILITIES
-
-;STATUS REG: BIT0 = CARRY, BIT2 = ZERO
-
-Zero:
-MOVF A
-DC, W
-SUBLW B'00011011'
-MOVF B'11111100', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO ONE
-
-ONE:
-MOVF ADC, W
-SUBLW B'00110010'
-MOVF B'01100000', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO TWO
-
-TWO:
-MOVF ADC, W
-SUBLW B'01010001'
-MOVF B'11011010', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO THREE
-
-THREE:
-MOVF ADC, W
-SUBLW B'01110101'
-MOVF B'11110010', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO FOUR
-
-FOUR:
-MOVF ADC, W
-SUBLW B'10001001'
-MOVF B'01100110', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO FIVE
-
-FIVE:
-MOVF ADC, W
-SUBLW B'10011110'
-MOVF B'10110110', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO SIX
-
-SIX:
-MOVF ADC, W
-SUBLW B'10101101'
-MOVF B'10111110', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO SEVEN
-
-SEVEN:
-MOVF ADC, W
-SUBLW B'11000010'
-MOVF B'11100000', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO EIGHT
-
-EIGHT:
-MOVF ADC, W
-SUBLW B'11010001'
-MOVF B'11111110', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO NINE
-
-NINE:
-MOVF ADC, W
-SUBLW B'11111111'
-MOVF B'11100110', D_BCD
-BTFSC STATUS, 0
-BTFSC STATUS, 2
-GOTO SET_BCD
-GOTO ERROR
-
-SET_BCD
-MOVF D_BCD, w
-MOVWF PORTB
-;********************************
-GOTO DONDESEAQUEHAYAQUELLAMARLO;*
-;********************************
-
 MOVEVALUE:	
 	call 	DELAY
 	MOVF	DATO, W  
-	MOVWF	PORTD	
+	MOVWF	PORTB	
 	RETURN
 CLEANY:
 	MOVLW 	0x00  ;se asigna la literal a w (0x00)
@@ -413,6 +332,7 @@ EJEY:
 	call	MVY9
 	RETURN
 REVERSEEJEY:
+	;call 	DELAY
 	call	MVY9
 	call	MVY3
 	call	MVY6
@@ -532,4 +452,261 @@ MVY12:
 	MOVWF	DATO
 	call	MOVEVALUE
 	RETURN
+;-----------------------------------------------------------------
+;							PHOTOCELL
+GET_PHOTOCELL:
+_bucle:
+	;btfss INTCON,T0IF
+	;goto _bucle ;Esperar que el timer0 desborde
+	; SE DEBE DE COLOCAR UN DELAY PARA QUE ESPERE LA CONVERSION
+	BSF  STATUS,Z
+	CALL _PRESPERA
+	bcf INTCON,T0IF ;Limpiar el indicador de desborde
+	bsf ADCON0,GO ;Comenzar conversion A/D
+_espera:
+	btfsc ADCON0,GO ;ADCON0 es 0? (la conversion esta completa?)
+	goto _espera ;No, ir _espera
+	movf ADRESH,W ;Si, W=ADRESH
+	; 1Eh ADRESH A/D Result Register High Byte
+	; 9Eh ADRESL A/D Result Register Low Byte 
+	movwf ADC ;ADC=W
+	movfw ADC ;W = ADC
+    	goto  V1
+returnExec:
+    MOVLW  	waffles ;DATO of Photocell
+    MOVWF 	TMPCELL
+	goto COMPARE
+
+V1:
+    movlw D'210'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V2
+    movlw 0x0
+    movwf waffles
+    call case0
+    goto returnExec
+
+V2:
+    movlw D'194'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V3
+    movlw 0x1
+    movwf waffles
+    call case1
+    goto returnExec
+
+V3:
+    movlw D'174'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V4
+    movlw 0x2
+    movwf waffles
+    call case2
+    goto returnExec
+
+V4:
+    movlw D'158'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V5
+    movlw 0x3
+    movwf waffles
+    call case3
+    goto returnExec
+
+V5:
+    movlw D'138'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V6
+    movlw 0x4
+    movwf waffles
+    call case4
+    goto returnExec
+
+V6:
+    movlw D'117'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V7
+    movlw 0x5
+    movwf waffles
+    call case5
+    goto returnExec
+
+V7:
+    movlw D'82'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V8
+    movlw 0x6
+    movwf waffles
+    call case6
+    goto returnExec
+
+V8:
+    movlw D'51'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V9
+    movlw 0x7
+    movwf waffles
+    call case7
+    goto returnExec
+
+V9:
+    movlw D'28'
+    subwf ADC, W
+    btfss STATUS, C
+    goto V10
+    movlw 0x8
+    movwf waffles
+    call case8
+    goto returnExec
+
+V10:
+    ; movlw D'0'
+    ; subwf ADC, W
+    ; btfss STATUS, N
+    ; goto V10<--
+    ;btfsc STATUS, C
+    movlw 0x9
+    movwf waffles
+    call case9
+    goto returnExec
+
+_PRESPERA:
+	MOVLW 0XFF
+	MOVWF CounterA
+	MOVWF COUNTERb
+	MOVWF COUNTERc	
+	CALL ESPE
+	RETURN	
+	
+ESPE:
+	DECFSZ	CounterA,0X01
+	GOTO	ESPE
+	CALL	ESPE2
+	RETURN
+ESPE2:
+	DECFSZ	COUNTERb,0X01
+	GOTO	ESPE2
+	CALL	ESPE3
+	RETURN
+ESPE3:
+	DECFSZ	COUNTERc,0X01
+	GOTO	ESPE3
+	RETURN	
+
+case0
+    bsf PORTA, 1
+    bsf PORTA, 2
+    bsf PORTA, 3
+    bsf PORTA, 4
+    bsf PORTA, 5
+    bsf PORTE, 0
+    bcf PORTE, 1
+    return 
+case1
+    ;movlw 0x0
+    bcf PORTA, 1
+    bcf PORTA, 4
+    bcf PORTA, 5
+    bcf PORTE, 0
+    bcf PORTE, 1
+    ;movlw 0x1
+    bsf PORTA, 2
+    bsf PORTA, 3
+    return
+case2
+    ;movlw 0x1
+    bsf PORTA, 1
+    bsf PORTA, 2
+    bsf PORTA, 4
+    bsf PORTA, 5
+    bsf PORTE, 1
+    ;movlw 0x0
+    bcf PORTA, 3
+    bcf PORTE, 0
+    return
+case3
+    ;movlw 0x1
+    bsf PORTA, 1
+    bsf PORTA, 2
+    bsf PORTA, 3
+    bsf PORTA, 4
+    bsf PORTE, 1
+    ;movlw 0x0
+    bcf PORTE, 0
+    bcf PORTA, 5
+    return
+case4
+    ;movlw 0x1
+    bsf PORTA, 2
+    bsf PORTA, 3
+    bsf PORTE, 0
+    bsf PORTE, 1
+    ;movlw 0x0
+    bcf PORTA, 1
+    bcf PORTA, 4
+    bcf PORTA, 5
+    return
+case5
+    ;movlw 0x1
+    bsf PORTA, 3
+    bsf PORTA, 4
+    bsf PORTE, 0
+    bsf PORTE, 1
+    bsf PORTA, 1
+    ;movlw 0x0
+    bcf PORTA, 5
+    bcf PORTA, 2
+    return
+case6
+    ;movlw 0x1
+    bsf PORTA, 3
+    bsf PORTA, 4
+    bsf PORTA, 5
+    bsf PORTE, 0
+    bsf PORTE, 1
+    bsf PORTA, 1
+    ;movlw 0x0
+    bcf PORTA, 2
+    return
+case7
+    ;movlw 0x1
+    bsf PORTA, 1
+    bsf PORTA, 2
+    bsf PORTA, 3
+    ;movlw 0x0
+    bcf PORTA, 4
+    bcf PORTA, 5
+    bcf PORTE, 0
+    bcf PORTE, 1
+    return
+case8
+    ;movlw 0x1
+    bsf PORTA, 1
+    bsf PORTA, 2
+    bsf PORTA, 3
+    bsf PORTA, 4
+    bsf PORTA, 5
+    bsf PORTE, 0
+    bsf PORTE, 1
+    return
+case9
+    ;movlw 0x1
+    bsf PORTA, 1
+    bsf PORTA, 2
+    bsf PORTA, 3
+    bsf PORTA, 4
+    bsf PORTE, 0
+    bsf PORTE, 1
+    ;movlw 0x0
+    bcf PORTA, 5
+    return
+
 END
